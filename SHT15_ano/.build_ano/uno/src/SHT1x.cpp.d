1a16a8f0e2b4731f62a4918525ef2ee2100bdd15
KD0SKH <patrick91jh@gmail.com>
2016-07-24 19:24:20 -0600

    first commit

---
+++ SHT15_ano/.build_ano/uno/src/SHT1x.cpp.d
@@ -0,0 +1,12 @@
+.build_ano/uno/src/SHT1x.cpp.d .build_ano/uno/src/SHT1x.o: src/SHT1x.cpp /usr/share/arduino/libraries/SHT15/avr/SHT1x.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/USBAPI.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/avr/variants/standard/pins_arduino.h
